badd76d9c27992a467ee2bc1e6cf648bc7e61787
Eduardo Titello <du.titello@gmail.com>
2020-08-08 16:32:00 -0300

    cleaning some old trash

--- test.py
+++
@@ -1,8 +0,0 @@
-from paransys2 import ANSYS
-ans = ANSYS()
-ans.setAPDLmodel('cant.inp')
-res = ans.solve()
-print(res)
-grad1 = ans.grad(b=4, h=1, e=90000, l=60)
-grad2 = ans.grad(b=4, h=1, e=90000, l=60, onlyfor=['b','h','e'], notfor=['e'])
-grad3 = ans.grad(b=4, h=1, e=90000, l=60, notfor=['b','h'])

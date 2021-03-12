a4ae1ebfa82f4998af2b1dd18476080686d01e46
dutitello <du.titello@gmail.com>
2021-03-12 09:02:51 -0300

    corrected init

--- paransys2/__init__.py
+++ paransys2/__init__.py
@@ -4,9 +4,3 @@ A new version of PARANSYS library.
 
 from paransys2.ansys import ANSYS
 from time import sleep
-
-if __name__ == '__main__':
-    ans = ANSYS()
-    ans.solve()
-    sleep(5)
-    ans.close()
